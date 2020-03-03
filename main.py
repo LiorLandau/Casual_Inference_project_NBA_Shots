0c7c7473edec89a755680cd4932375dc2106681e
lland <l.landau8@gmail.com>
2020-03-03 16:23:12 +0200

    change stratification

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 import warnings
 import numpy as np
 import pandas as pd
-# import copy
+import copy
 from scipy.spatial.distance import euclidean
 from sklearn.linear_model import LinearRegression, LogisticRegression, Ridge
 from sklearn.preprocessing import PolynomialFeatures
@@ -51,6 +51,7 @@ def propensity_score_LR(df):  # calculates propensity score by GLM estimate
 
 ### need to fix below###
 
+
 def S_learner(df):  # calculates ATE by S-learner
     df_copy = df.copy()
     X = df_copy.iloc[:, :-1]
@@ -83,8 +84,8 @@ def Matching(df):  # calculates ATE by matching
     t_0 = df[df['T'] == 0].drop(["T"], axis=1).values
     t_2 = []
     for row in t_1:
-        min_dist = 10000000000000  # heuristically
-        val = 100000000000000  # heuristically
+        min_dist = 10000000000000
+        val = 100000000000000
         for sub_row in t_0:
             dist = euclidean(row[:-1], sub_row[:-1])
             if dist < min_dist:
@@ -115,7 +116,6 @@ def get_group_effect(data, is_treatment=1, is_rct=1):
     group_record_size = data[data['T'] == is_treatment].shape[0]
     n = data.shape[0]
     group_effect = -1
-
     if (group_record_size >= THRESHOLD_S_IPW):
         if is_rct:
             if is_treatment:
@@ -133,10 +133,8 @@ def get_group_effect(data, is_treatment=1, is_rct=1):
 def inverse_propensity_score(data, is_rct=1):
     treatment = 1
     control = 0
-
     treatment_effect = get_group_effect(data, treatment, is_rct)
     control_effect = get_group_effect(data, control, is_rct)
-
     if (treatment_effect != -1 and control_effect != -1):
         ate = treatment_effect - control_effect
     else:
@@ -145,13 +143,13 @@ def inverse_propensity_score(data, is_rct=1):
 
 
 ######################################################### Other team project#############################################################
+
+
 def propensity_score_lr_predictor(X, T):
     clf = LogisticRegression(random_state=0, max_iter=10000, solver='lbfgs', multi_class='multinomial').fit(X, T)
     ps_score = np.asarray(clf.predict_proba(X))
-
     T_pred = clf.predict(X)
     # print('Accuracy LR: ' + str(accuracy_score(T,T_pred)))
-
     return ps_score
 
 
@@ -160,46 +158,33 @@ def ridge_linear_regression(adj_bin_data, with_ps=True):
         X = adj_bin_data.drop(columns=['Y', 'ps_0', 'ps_1'])
     else:
         X = adj_bin_data.drop(columns=['Y'])
-
     y = adj_bin_data['Y']
-
     ridge = Ridge(random_state=0)
-
     clf = ridge.fit(X, y)
     T_coef = clf.coef_[-1]
-
     y_pred = clf.predict(X)
     # y_pred_T = clf.predict(X[X["T"]==1])
     # y_pred_C = clf.predict(X[X["T"]==0])
-
     # ate = (y_pred_T-y_pred_C).mean()
-
     # print('RMSE: ' + str(math.sqrt(mean_squared_error(y,y_pred))))
-
     # t_ind,p_value = stats.ttest_ind(y,y_pred,equal_var=False)
     # print ('linear_t: ' + str (t_ind) + ' linear_p: '+ str(p_value))
-
     return T_coef, y_pred
 
 
 def ridge_non_linear_regression(adj_bin_data):
     X = adj_bin_data.drop(columns=['Y', 'ps_0', 'ps_1'])
     y = adj_bin_data['Y']
-
     columns = X.columns
-
     ridge = Ridge(random_state=0)
     poly = PolynomialFeatures(2)
     X_poly = poly.fit_transform(X)
-
     clf = ridge.fit(X_poly, y)
     features_coef = dict(zip(poly.get_feature_names(columns), clf.coef_))
     T_coef_non_linear = features_coef['T']
-
     y_pred = clf.predict(X_poly)
     # t_ind,p_value = stats.ttest_ind(y,y_pred,equal_var=False)
     # print ('non_linear_t: ' + str (t_ind) + ' non_linear_p: '+ str(p_value))
-
     # print('RMSE_non_linear: ' + str(math.sqrt(mean_squared_error(y,y_pred))))
     return T_coef_non_linear, y_pred
 
@@ -207,22 +192,17 @@ def ridge_non_linear_regression(adj_bin_data):
 def ridge_non_linear_regression_robust(adj_bin_data):
     X = adj_bin_data.drop(columns=['Y', 'ps_0', 'ps_1'])
     columns = X.columns
-
     poly = PolynomialFeatures(2)
     X_poly = poly.fit_transform(X)
     X_poly = pd.DataFrame(data=X_poly, columns=poly.get_feature_names(columns))
     X_poly_treated = X_poly[X_poly['T'] == 1]
     X_poly_control = X_poly[X_poly['T'] == 0]
-
     y_treated = adj_bin_data[adj_bin_data['T'] == 1]['Y']
     y_control = adj_bin_data[adj_bin_data['T'] == 0]['Y']
-
     ridge_treated = Ridge(random_state=0)
     ridge_control = Ridge(random_state=1)
-
     clf_treated = ridge_treated.fit(X_poly_treated, y_treated)
     clf_control = ridge_control.fit(X_poly_control, y_control)
-
     y_pred_treated = clf_treated.predict(X_poly)
     y_pred_control = clf_control.predict(X_poly)
     return y_pred_treated, y_pred_control
@@ -238,20 +218,17 @@ def calc_doubly_robust_sum(bin_data):
 
     left_hand_sum = ((T * y / ps) - ((T - ps) / ps) * treatment_prediction).sum()
     right_hand_sum = ((((1 - T) * y) / (1 - ps)) + ((T - ps) / (1 - ps)) * control_prediction).sum()
-    sum = (left_hand_sum - right_hand_sum) / n
-    return sum
+    sum_answer = (left_hand_sum - right_hand_sum) / n
+    return sum_answer
 
 
 def doubly_robust_estimator(bin_data):
     bin_data_treated = bin_data[bin_data['T'] == 1]
     bin_data_control = bin_data[bin_data['T'] == 0]
-
     if (bin_data_treated.shape[0] >= THRESHOLD_S_DR and bin_data_control.shape[0] >= THRESHOLD_S_DR):
         treated_predictions, control_predictions = ridge_non_linear_regression_robust(bin_data)
-
         bin_data['treated_prediction'] = treated_predictions
         bin_data['control_prediction'] = control_predictions
-
         ate = calc_doubly_robust_sum(bin_data)
     else:
         ate = np.nan
